46db5ce7318912a5633eb3061a7f771f4cdec376
Daniil Efremov <daniil.efremov98@gmail.com>
2019-10-03 15:18:52 +0200

    Added median-function and test_median_singleton()

--- src/daniil_efremov_ex/ex03/test_median.py
+++ src/daniil_efremov_ex/ex03/test_median.py
@@ -4,3 +4,18 @@ __author__ = 'Daniil Efremov'
 __email__ = 'daniil.vitalevich.efremov@nmbu.no'
 
 
+import pytest
+
+
+def median(data):
+    sort_data = sorted(data)
+    num_el = len(sort_data)
+
+    if num_el % 2 == 1:
+        return sort_data[num_el // 2]
+    else:
+        return (sort_data[num_el // 2 - 1] + sort_data[num_el // 2]) / 2
+
+
+def test_median_of_singleton():
+    assert median([4]) == 4
