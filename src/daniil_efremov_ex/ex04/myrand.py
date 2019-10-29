170b97d9295c4d222bde4e2441650f46aaf7c2e9
Daniil Efremov <daniil.efremov98@gmail.com>
2019-10-29 10:37:36 +0100

    Added code for files

--- src/daniil_efremov_ex/ex04/myrand.py
+++ src/daniil_efremov_ex/ex04/myrand.py
@@ -2,3 +2,33 @@
 
 __author__ = 'Daniil Efremov'
 __email__ = 'daniil.vitalevich.efremov@nmbu.no'
+
+
+class LSGRand:
+    def __init__(self, seed):
+        self.seed = seed
+
+    def rand(self):
+        a = 16807
+        m = 2 ** 31 - 1
+        self.seed = a * self.seed % m
+
+        return self.seed
+
+
+class ListRand:
+    def __init__(self, nummer_liste):
+        self.nummer_liste = nummer_liste
+        self.nr_i_lista = 0
+
+    def rand(self):
+
+        if self.nr_i_lista >= len(self.nummer_liste):
+            raise RuntimeError
+
+        else:
+
+            neste_nummer = self.nummer_liste[self.nr_i_lista]
+            self.nr_i_lista += 1
+
+            return neste_nummer
