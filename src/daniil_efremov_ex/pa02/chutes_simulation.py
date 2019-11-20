965e95410e7cec90bf662453cc3983e41395c5d0
Sigurd Grøtan <sgrotan@nmbu.com>
2019-11-20 14:39:18 +0100

    Added basic code skeleton in chutes_simulation

--- src/daniil_efremov_ex/pa02/chutes_simulation.py
+++ src/daniil_efremov_ex/pa02/chutes_simulation.py
@@ -2,3 +2,12 @@
 
 __author__ = 'Daniil Efremov', 'Sigurd Grøtan'
 __email__ = 'daniil.vitalevich.efremov@nmbu.no', 'sgrotan@nmbu.no'
+
+class Board:
+    pass
+
+class Player:
+    pass
+
+class Simulation:
+    pass
