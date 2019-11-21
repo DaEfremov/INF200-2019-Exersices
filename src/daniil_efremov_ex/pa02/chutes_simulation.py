7ff031011f0145950b00e0325e671e3f071cee46
Daniil Efremov <daniil.efremov98@gmail.com>
2019-11-21 16:31:42 +0100

    Started adding doc-strings and made changes according to PEP8

--- src/daniil_efremov_ex/pa02/chutes_simulation.py
+++ src/daniil_efremov_ex/pa02/chutes_simulation.py
@@ -4,28 +4,36 @@ __author__ = 'Daniil Efremov', 'Sigurd Grøtan'
 __email__ = 'daniil.vitalevich.efremov@nmbu.no', 'sgrotan@nmbu.no'
 
 from random import randint, shuffle
-#default_ladders = [(1,40), (8,10), (36,52), (43,62),
- #                               (49,79), (65,82), (68,85)]
-  #  default_chutes = [(24,5), (33,3), (42,30), (56,37), (64,27),
-  #                              (74,12), (87,70)]
+
 
 class Board:
-    def __init__(self, ladders=[(1,40), (8,10), (36,52), (43,62),
-                                (49,79), (65,82), (68,85)],
-                        chutes=[(24,5), (33,3), (42,30), (56,37), (64,27),
-                                (74,12), (87,70)], goal=90):
+    """Class that creates the board"""
+    default_ladders = [(1, 40), (8, 10), (36, 52), (43, 62),
+                       (49, 79), (65, 82), (68, 85)]
+    default_chutes = [(24, 5), (33, 3), (42, 30), (56, 37), (64, 27),
+                      (74, 12), (87, 70)]
+
+    def __init__(self, ladders=None, chutes=None, goal=90):
+        if ladders is None:
+            ladders = Board.default_ladders
+        if chutes is None:
+            chutes = Board.default_chutes
         self.ladders = ladders
         self.chutes = chutes
         self.goal = goal
 
     def goal_reached(self, position):
+        """Checks if player has reached the desired position
+        Returns True or False"""
         if position >= self.goal:
             return True
         else:
             return False
 
-
     def position_adjustment(self, position):
+        """Checks if the position the player
+        reached has a chute or a ladder on it
+        Returns position"""
         for ladder in self.ladders:
             if position == ladder[0]:
                 position = ladder[1]
@@ -38,38 +46,46 @@ class Board:
 
 
 class Player:
+    """Class that creates the player"""
 
     def __init__(self, board):
         self.board = board
         self.position = 0
         self.adjusted_position = 0
 
-
     def move(self):
+        """Makes the player do a move,
+        and adjustes to the correct end-position"""
         self.position = self.adjusted_position
         self.position += randint(1, 6)
         self.adjusted_position = self.board.position_adjustment(self.position)
 
 
 class ResilientPlayer(Player):
+    """Class with different set of rules for when player hits a chute"""
 
     def __init__(self, board, extra_steps=1):
         self.extra_steps = extra_steps
         super().__init__(board)
 
     def move(self):
+        """Makes the player move extra steps
+        forward after sliding down a chute"""
         super().move()
         if self.position > self.adjusted_position:
             self.adjusted_position += self.extra_steps
 
 
 class LazyPlayer(Player):
+    """Class with different set of rules for when a players hits a ladder"""
 
     def __init__(self, board, dropped_steps=1):
         self.dropped_steps = dropped_steps
         super().__init__(board)
 
     def move(self):
+        """Makes the player move back some steps after
+        climbing up a ladder"""
         super().move()
         if self.position < self.adjusted_position:
             if (self.adjusted_position - self.dropped_steps) <= self.position:
@@ -78,10 +94,12 @@ class LazyPlayer(Player):
                 self.adjusted_position -= self.dropped_steps
 
 
-
 class Simulation:
+    """Class that simulates a number of games of Chutes and Ladders"""
 
-    def __init__(self, player_field=[Player, Player], board=Board(), seed=123, randomize_players=True):
+    def __init__(self, player_field=[Player, Player], board=Board(), seed=123,
+                 randomize_players=True):
+
         self.player_field = player_field
         self.board = board
         self.seed = seed
@@ -107,7 +125,6 @@ class Simulation:
 
         return winner
 
-
     def run_simulation(self, num_sim):
         for _ in range(num_sim):
             self.winner_list.append(self.single_game())
@@ -116,7 +133,8 @@ class Simulation:
         return self.winner_list
 
     def durations_per_type(self):
-        durations_dict = {'Player': [], 'LazyPlayer': [], 'ResilientPlayer': []}
+        durations_dict = {'Player': [], 'LazyPlayer': [],
+                          'ResilientPlayer': []}
 
         for player in self.result_list:
             durations_dict[player[1]].append(player[0])
@@ -139,7 +157,9 @@ class Simulation:
 
         return players_dict
 
-sim = Simulation([Player, Player, ResilientPlayer, ResilientPlayer,ResilientPlayer, LazyPlayer])
+
+sim = Simulation([Player, Player, ResilientPlayer, ResilientPlayer,
+                  ResilientPlayer, LazyPlayer])
 
 sim.run_simulation(5)
 print(sim.winners_per_type())
