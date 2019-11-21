df8a7757accf695860e7c82ed94c93592a614cf5
Daniil Efremov <daniil.efremov98@gmail.com>
2019-11-21 01:14:00 +0100

    Created more specific code according to exersice.

--- src/daniil_efremov_ex/pa02/chutes_simulation.py
+++ src/daniil_efremov_ex/pa02/chutes_simulation.py
@@ -4,43 +4,76 @@ __author__ = 'Daniil Efremov', 'Sigurd Grøtan'
 __email__ = 'daniil.vitalevich.efremov@nmbu.no', 'sgrotan@nmbu.no'
 
 from random import randint
-
+#default_ladders = [(1,40), (8,10), (36,52), (43,62),
+ #                               (49,79), (65,82), (68,85)]
+  #  default_chutes = [(24,5), (33,3), (42,30), (56,37), (64,27),
+  #                              (74,12), (87,70)]
 
 class Board:
-
-    def __init__(self, ladders=[(1, 4), (5, 16)], chutes=[(9, 2), (12, 3)], goal=90):
+    def __init__(self, ladders=[(1,40), (8,10), (36,52), (43,62),
+                                (49,79), (65,82), (68,85)],
+                        chutes=[(24,5), (33,3), (42,30), (56,37), (64,27),
+                                (74,12), (87,70)], goal=90):
         self.ladders = ladders
         self.chutes = chutes
         self.goal = goal
 
-    def goal_reached(self, player):
-        return False
+    def goal_reached(self, position):
+        if position >= self.goal:
+            return True
 
-    def position_adjustment(self, pos):
-        return pos + randint(1, 6)
+
+    def position_adjustment(self, position):
+        for ladder in self.ladders:
+            if position == ladder[0]:
+                position = ladder[1]
+
+        for chute in self.chutes:
+            if position == chute[0]:
+                position = chute[1]
+
+        return position
 
 
 class Player:
 
     def __init__(self, board):
         self.board = board
+        self.position = 0
+        self.adjusted_position = 0
 
     def move(self):
-        pass
+        self.position = self.adjusted_position
+        self.position += randint(1, 6)
+        self.adjusted_position = self.board.position_adjustment(self.position)
 
 
 class ResilientPlayer(Player):
 
-    def __init__(self, board, extra_steps=4):
+    def __init__(self, board, extra_steps=1):
         self.extra_steps = extra_steps
         super().__init__(board)
+
+    def move(self):
+        super().move()
+        if self.position > self.adjusted_position:
+            self.adjusted_position += self.extra_steps
 
 
 class LazyPlayer(Player):
 
-    def __init__(self, board, dropped_steps=3):
+    def __init__(self, board, dropped_steps=1):
         self.dropped_steps = dropped_steps
         super().__init__(board)
+
+    def move(self):
+        super().move()
+        if self.position < self.adjusted_position:
+            if (self.adjusted_position - self.dropped_steps) <= self.position:
+                self.adjusted_position = self.position
+            else:
+                self.adjusted_position -= self.dropped_steps
+
 
 
 class Simulation:
