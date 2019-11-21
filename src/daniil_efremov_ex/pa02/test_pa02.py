906f3d0164bc4ad156e0eb155bb70585ef0c33c0
Sigurd Grøtan <sgrotan@nmbu.com>
2019-11-21 18:38:17 +0100

    Added a some new tests.

---
+++ src/daniil_efremov_ex/pa02/test_pa02.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Daniil Efremov', 'Sigurd Grøtan'
+__email__ = 'daniil.vitalevich.efremov@nmbu.no', 'sgrotan@nmbu.no'
+
+import chutes_simulation as cs
+
+
+class TestBoard:
+    """
+    Tests for Board class.
+    """
+
+    def test_goal_reached(self):
+
+        position_1 = 80
+        position_2 = 100
+        b = cs.Board(goal=90)
+        assert b.goal_reached(position_1) is True
+        assert b.goal_reached(position_2) is False
+
+    def test_position_adjustment(self):
+
+        position_1 = 1
+        position_2 = 33
+        position_3 = 20
+        b = cs.Board(ladders=[(1, 40)], chutes=[(33, 3)])
+        assert b.position_adjustment(position_1) == 40
+        assert b.position_adjustment(position_2) == 3
+        assert b.position_adjustment(position_3) == position_3
+
+
+class TestPlayer:
+
+    def test_move(self):
+
+        p = cs.Player(cs.Board())
+        pre_pos = p.position
+        p.move()
+        post_pos = p.position
+        assert pre_pos != post_pos
+
+    def test_move_less_than_1(self):
+
+        for _ in range(10000):
+            p = cs.Player(cs.Board())
+            p.move()
+            assert p.position > 0
+
+    def test_move_not_chute_or_ladder(self):
+
+        for _ in range(10000):
+            p = cs.Player(cs.Board(ladders=[(1, 40)], chutes=[(33, 3)]))
+            for _ in range(100):
+                p.move()
+                assert p.adjusted_position != 1 and p.adjusted_position != 33
+
+
+class TestSimulation:
+
+    def test_single_game(self):
+        pass
+
+    def test_run_simulation(self):
+        pass
+
+    def test_get_results(self):
+        pass
+
+    def test_players_per_type(self):
+        pass
+
+    def test_winners_per_type(self):
+        pass
+
+    def test_durations_per_type(self):
+        pass
